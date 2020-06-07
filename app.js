10d118e0e47f8938d242e998ddf482a75125f3b0
carranj <carranco.jose.r@gmail.com>
2020-06-07 10:30:53 -0500

    Error Handling in Express

--- app.js
+++ app.js
@@ -12,6 +12,24 @@ app.use(jsonParser());
 
 app.use("/questions", routes);
 
+//catch 404 and forward to error
+app.use(function(req, res, next){
+    var err = new Error('Page Not Found');
+    err.status = 404;
+    next(err);
+})
+
+//Error Handler
+
+app.use(function(err, req, res, next){
+    res.status(err.status || 500); 
+    res.json({
+        error: {
+            message: err.message
+        }
+    });
+});
+
 var port = process.env.PORT || 3000;
 
 app.listen(port, function(){
--- routes.js
+++ routes.js
@@ -22,7 +22,7 @@ router.post("/", function(req, res){
 //Route for specific questions
 router.get("/:qID", function(req, res){
     res.json({
-        response: `You sent me a GET request for ${req.params.qID}`
+        response: `You sent me a GET request for ID ${req.params.qID}`
     }); 
  });
 
@@ -40,7 +40,7 @@ router.post("/:qID/answers", function(req, res){
 //Edit a  specific answer
 router.put("/:qID/answers/:aID", function(req, res){
     res.json({
-        response: "You sent me a POST request to /answers",
+        response: "You sent me a PUT request to /answers",
         questionId: req.params.qID,
         answerId: req.params.aID,
         body: req.body
@@ -60,7 +60,15 @@ router.delete("/:qID/answers/:aID", function(req, res){
  //POST /questions/:qID/answers/:aID/vote-up
   //POST /questions/:qID/answers/:aID/vote-down
 //Vote on a specific answer
-router.post("/:qID/answers/:aID/vote-:dir", function(req, res){
+router.post("/:qID/answers/:aID/vote-:dir", function(req, res, next){
+        if(req.params.dir.search(/^(up|down)$/) === -1){
+            var err = new Error("Not Found");
+            err.status = 404;
+            next(err);
+        } else {
+            next();
+        }
+    },function(req, res){
     res.json({
         response: `You sent me a POST request to /vote-${req.params.dir}`,
         questionId: req.params.qID,
@@ -68,6 +76,5 @@ router.post("/:qID/answers/:aID/vote-:dir", function(req, res){
         vote: req.params.dir
     }); 
  });
-
 
 module.exports = router;

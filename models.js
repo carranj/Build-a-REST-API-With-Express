3363ed15388e3f10c27f1c7aaafccad921ce906e
carranj <carranco.jose.r@gmail.com>
2020-06-10 00:23:08 -0500

    Extending Mongoose

--- models.js
+++ models.js
@@ -4,6 +4,16 @@ var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
 
+var sortAnswers = function(a,b){
+    // (-) if a before b
+    // 0 no order change
+    // (+) if a after b
+    if(a.votes === b.votes){
+        return b.updatedAt - a.updatedAt;
+    }
+    return b.votes - a.votes;
+}
+
 var AnswersSchema =new Schema({
     text: String,
     createdAt:{type:Date, default: Date.now},
@@ -11,10 +21,29 @@ var AnswersSchema =new Schema({
     votes: {type:Number, default:0}    
 });
 
+AnswersSchema.method("update", function(updates, callback){
+    Object.assign(this, updates, {updatedAt: new Date()});
+    this.parent().save(callback);
+});
+
+AnswersSchema.method("vote", function(updates, callback){
+    if(vote === "up"){
+        this.votes += 1;
+    } else {
+        this.votes -= 1;
+    }
+    this.parent().save(callback);
+});
+
 var QuestionSchema = new Schema({
     text: String,
     createdAt:{type:Date, default: Date.now},
     answers:[AnswersSchema]
+});
+
+QuestionSchema.pre("save",function(next){
+    this.answers.sort();
+    next();
 });
 
 var Question = mongoose.model("Question", QuestionSchema);
